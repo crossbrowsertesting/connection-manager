21fed193a22905c01995e73de6c0203faccbbe46
John Reese <johnr@crossbrowsertesting.com>
2017-08-17 10:10:11 -0500

    Move handlers under connect event

--- client.js
+++ client.js
@@ -22,14 +22,13 @@ var argv = require('yargs')
   })
   .argv;
 
-console.log("starting");
-
 function getApiUrl(env){
   switch(env){
     case 'prod':
       return 'https://crossbrowsertesting.com/tunnelserver'
     case 'test':
       return 'https://test.crossbrowsertesting.com/tunnelserver'
+      // return 'http://10.191.1.5:3000/tunnelserver'
     case 'local':
       return 'http://localhost:3000/tunnelserver'
   }
@@ -42,39 +41,70 @@ var socket = socketIo(getApiUrl(argv.env),
       reconnect: true }
 );
 
-socket.on('keepalive_check', () => {
-  socket.emit('keepalive_ack');
+
+socket.on('error', (err) => {
+  console.log("Socket error!!");
+  console.log(err.stack);
+})
+
+socket.on('disconnect', () => {
+  console.log("Socket disconnected");
+})
+
+socket.on('reconnect', () => {
+  console.log('Socket reconnected!');
+})
+
+socket.on('reconnect_attempt', () => {
+  console.log('Socket reconnecting...');
+})
+
+socket.on('reconnecting', () => {
+  console.log('Socket reconnecting...');
+})
+
+socket.on('reconnect_error', (err) => {
+  console.log('reconnect error: ' + err);
+})
+
+socket.on('reconnect_failed', (err) => {
+  console.log('reconnect failed');
 })
 
 socket.on('connect', () => {
   console.log("connection established! Initiating auth!");
   socket.emit('authenticate', {username: argv.username, authkey: argv.authkey});
+  socket.on('authenticated', function() {
+    // `authenticated` is emitted by socketio-auth server 
+    console.log("authentication successful!");
+    // setup the other socket message handlers
+    socket.on('start', (msg) => {
+      console.log("got a start message! time to start a tunnel....");
+      console.log("message is: " + msg)
+      msg = JSON.parse(msg);
+      // start the tunnel!
+      if (argv.env == 'test' || argv.env == 'local'){
+        msg.options.test = true;
+      }
+      new Tunnel(msg.user.username, msg.user.authkey, msg.options).start()
+    })
+
+    socket.on('keepalive_check', () => {
+      socket.emit('keepalive_ack');
+    })
+
+    socket.on('unauthorized', function(err) {
+      // `unauthorized` is emitted by socketio-auth server when auth fails
+      // connection is closed by the server
+      console.log("authentication failed! Reason: " + err);
+    });
+
+    socket.on('bye', () => {
+      console.log("got bye");
+      socket.disconnect();
+    })
+  });
 })
 
-socket.on('authenticated', function() {
-  // `authenticated` is emitted by socketio-auth server 
-  console.log("authentication successful!");
-  // setup the other socket message handlers
-  socket.on('start', (msg) => {
-    console.log("got a start message! time to start a tunnel....");
-    console.log("message is: " + msg)
-    msg = JSON.parse(msg);
-    // start the tunnel!
-    if (argv.env == 'test' || argv.env == 'local'){
-      msg.options.test = true;
-    }
-    new Tunnel(msg.user.username, msg.user.authkey, msg.options).start()
-  })
-
-  socket.on('bye', () => {
-    console.log("got bye");
-    socket.disconnect();
-  })
-});
   
-socket.on('unauthorized', function(err) {
-  // `unauthorized` is emitted by socketio-auth server when auth fails
-  // connection is closed by the server
-  console.log("authentication failed! Reason: " + err);
-});
 
