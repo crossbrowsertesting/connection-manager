63d7d827fbc076e09dd1273e551b69fee0ce8526
John Reese <johnreese.vt@gmail.com>
2019-08-27 16:24:02 -0500

    migrated to ws-based signaling tunnel and added proxy support

--- cbt-enterprise-connection-manager.js
+++ cbt-enterprise-connection-manager.js
@@ -1,9 +1,11 @@
 #!/usr/local/bin/node
 "use strict";
-var socketIo = require('socket.io-client');
+var WebSocket = require('ws')
 var http = require('http');
+var url = require('url');
 var Tunnel = require('./tunnel');
 var utils = require('./utils');
+var ProxyAgent = require('https-proxy-agent');
 
 // simple arg parser
 // args are stored in argv object
@@ -18,25 +20,28 @@ var argv = require('yargs')
     'env': {
       description: false,
       default: 'prod'
-    }
+    },
 
   })
   .argv;
+
+var httpProxy  = process.env.http_proxy  || process.env.HTTP_PROXY  || null
+var httpsProxy = process.env.https_proxy || process.env.HTTPS_PROXY || null
 
 var tunnels = [];
 
 function getApiUrl(env){
   switch(env){
     case 'prod':
-      return 'https://crossbrowsertesting.com/localconman';
+      return 'https://livetestdirect.crossbrowsertesting.com/localconmanws';
     case 'qa':
-      return 'https://qaapp.crossbrowsertesting.com/localconman';
+      return 'https://qaapp.crossbrowsertesting.com/localconmanws';
     case 'test':
-      return 'https://test.crossbrowsertesting.com/localconman'
+      return 'https://test.crossbrowsertesting.com/localconmanws'
       // return 'https://testapp2.crossbrowsertesting.com/localconman'
       // return 'http://173.12.250.100/localconman';
     case 'local':
-      return 'http://localhost:3000/localconman';
+      return 'http://localhost:3000/localconmanws';
   }
 }
 
@@ -46,13 +51,23 @@ utils.checkVersion( argv.env, () => {
 
   // var socket = socketIo("http://localhost:3000/socket", { path: "/socket"} );
   // var socket = socketIo.connect("http://localhost:3000/socket/socket");
-  var socket = socketIo(getApiUrl(argv.env),
-    { path: "/api/v3/socket.io",
-      // need to make sure that only websockets protocol is used
-      transports: ['websocket'], 
-      upgrade: false
-    }
-  );
+  var proxy = httpProxy || httpsProxy
+
+  if(proxy){
+      console.log('going to setup proxy agent')
+      var proxyURL = new url.URL(proxy)
+      var proxyAgentOpts ={
+          host:proxyURL.hostname,
+          port: proxyURL.port,
+          auth: proxyURL.username ? `${proxyURL.username}:${proxyURL.password}` : ``,
+          secureProxy:true
+      }
+      console.log('proxy agent opts: ' + JSON.stringify(proxyAgentOpts))
+      var proxyAgent = new ProxyAgent(proxyAgentOpts)
+      var socket = new WebSocket(getApiUrl(argv.env), {agent: proxyAgent});
+  } else {
+      var socket = new WebSocket(getApiUrl(argv.env));
+  }
 
 
   socket.on('error', (err) => {
@@ -66,7 +81,7 @@ utils.checkVersion( argv.env, () => {
 
   socket.on('reconnect', () => {
     console.log("connection re-established! Re authing!");
-    socket.emit('authenticate', {username: argv.username, authkey: argv.authkey});
+    socket.send(JSON.stringify( {action: 'authenticate', username: argv.username, authkey: argv.authkey}));
   })
 
   socket.on('reconnecting', () => {
@@ -82,48 +97,52 @@ utils.checkVersion( argv.env, () => {
   })
 
 
-  socket.once('connect', () => {
+  socket.once('open', () => {
     console.log("connection established! Initiating auth!");
-    socket.emit('authenticate', {username: argv.username, authkey: argv.authkey});
+    socket.send(JSON.stringify({action: 'authenticate', username: argv.username, authkey: argv.authkey}));
 
-    socket.on('authenticated', function() {
-      // `authenticated` is emitted by socketio-auth server 
-      console.log("Authentication successful! Waiting for a request to open a tunnel...");
-      // setup the other socket message handlers
-    });
+    socket.on('message', msg => {
+        // handle incoming messages through the signaling socket
+        try {
+            msg = JSON.parse(msg)
+        } catch (parseErr) {
+            console.error('could not parse inbound message: ' + msg)
+            return
+        }
 
-    socket.on('start', (msg) => {
-      msg = JSON.parse(msg);
-      // start the tunnel!
-      if (argv.env == 'test' || argv.env == 'local'){
-        msg.options.test = 'test';
-      }
-      var t = new Tunnel(msg.user.username, msg.user.authkey, msg.options)
-      tunnels.push(t)
-      t.start()
-    });
-
-    socket.on('keepalive_check', () => {
-      socket.emit('keepalive_ack');
-    });
-
-    socket.on('unauthorized', function(err) {
-      // `unauthorized` is emitted by socketio-auth server when auth fails
-      // connection is closed by the server
-      console.log("authentication failed! Reason: " + err);
-    });
-
-    socket.on('bye', () => {
-      console.log("got bye");
-      socket.disconnect();
-    });
-
+        // after parsing successful, each message should come in with an "action"
+        switch(msg.action){
+            case 'authenticated':
+                console.log("Authentication successful! Waiting for a request to open a tunnel...");
+                break
+            case 'start':
+                if (argv.env == 'test' || argv.env == 'local'){
+                    msg.options.test = 'test';
+                }
+                var t = new Tunnel(msg.user.username, msg.user.authkey, msg.options)
+                tunnels.push(t)
+                t.start()
+                break
+            case 'keepalive_check':
+                socket.send(JSON.stringify({action:'keepalive_ack'}))
+                break
+            case 'unauthorized':
+                console.error("authentication failed! " + msg.message)
+                process.exit(1)
+                break
+            case 'bye':
+                console.log("got bye");
+                socket.close();
+        }
+    })
   })
 
 
   process.on('SIGINT', () => {
     console.log('\nECM is shutting down');
-    console.dir(tunnels)
+    if (tunnels.length === 0){
+        process.exit(0)
+    }
     tunnels.map( (tunnel, tunnelIndex) => {
       tunnel.stop( () => {
       })
